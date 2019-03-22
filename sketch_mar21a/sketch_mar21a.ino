bb8a5c0a4ed6d2c561a145041a4165294a0d972a
andrescimma1 <47437476+andrescimma1@users.noreply.github.com>
2019-03-21 21:56:13 -0300

    Prender y apagar la luz led cada 1 segundo

---
+++ sketch_mar21a/sketch_mar21a.ino
@@ -0,0 +1,15 @@
+void setup() //Se ejecuta una sola vez
+{
+  
+  pinMode(13, OUTPUT);  //Recibe dos cosas, un pin y un modo
+
+}
+
+void loop() 
+{
+
+  digitalWrite(13, HIGH); //Prender el pin 13, el "HIGH" sirve para prender, el "LOW" para apagar.
+  delay(1000);  // 1000ms = 1s
+  digitalWrite(13, LOW);
+  delay(1000);
+}
